c60b1bdaf38aa0262e2ea27bd1718944a9e073aa
himanshu-tyd <himanshu-tyd>
2022-12-29 00:55:58 +0530

    Updates

---
+++ precache-manifest.97fffbc377e7322705309a679db9cd01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30ba0d6bcedd9921f434",
+    "url": "/Monster-rodolex/static/css/main.e005d3a5.chunk.css"
+  },
+  {
+    "revision": "30ba0d6bcedd9921f434",
+    "url": "/Monster-rodolex/static/js/main.30ba0d6b.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/Monster-rodolex/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "ebd5b217692bce4a6d85",
+    "url": "/Monster-rodolex/static/js/2.ebd5b217.chunk.js"
+  },
+  {
+    "revision": "0ff6be6adba66bcc3d39",
+    "url": "/Monster-rodolex/static/js/runtime~main.0ff6be6a.js"
+  },
+  {
+    "revision": "57dcd64a4d141c6f9c1c876c953551fb",
+    "url": "/Monster-rodolex/index.html"
+  }
+];
